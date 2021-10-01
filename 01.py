7789e75074bbe26eb68f50394d1ebb9e531400ff
AYurau <90280657+AYurau@users.noreply.github.com>
2021-10-01 19:31:57 +0300

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,8 +1,8 @@
 def credit_card(number):
     if len(number) != 16:
-        return 'Неверный номер карты'
+        return f'Неверный номер карты'
     else:
-        return'**** **** ****', number[12:16]
+        return f'**** **** ****', number[12:16]
 
 
 credit_card('9112000049873221')
